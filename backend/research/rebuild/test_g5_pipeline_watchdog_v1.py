3edf091552a8a82e2e5bbe183d9b112515ea02ea
leegkssk2000-commits <leegkssk2000@gmail.com>
2026-09-04 18:31:55 +0200

    G5: make watchdog tests dependency-free

--- backend/research/rebuild/test_g5_pipeline_watchdog_v1.py
+++ backend/research/rebuild/test_g5_pipeline_watchdog_v1.py
@@ -1,22 +1,15 @@
 from backend.research.rebuild.g5_pipeline_watchdog_v1 import funnel
 
 
-def test_signal_starvation():
+def run() -> None:
     assert funnel({'raw_signal_count':0},{})['classification']=='SIGNAL_STARVATION'
-
-
-def test_admission_rejecting_all_raw_signals():
     assert funnel({'raw_signal_count':3,'collector_actionable_signal_count':0},{})['classification']=='ADMISSION_REJECTING_ALL_RAW_SIGNALS'
-
-
-def test_live_maturing():
     assert funnel({'collector_actionable_signal_count':2,'maturing_count':2},{})['classification']=='SIGNAL_LIVE_MATURING'
-
-
-def test_closed_candidate_waiting_settlement():
     assert funnel({'closed_candidate_count':1},{})['classification']=='CLOSED_CANDIDATE_WAITING_SETTLEMENT_OR_WRITER'
-
-
-def test_negative_economics():
     x=funnel({}, {'closed_T':5,'metrics':{'net_pnl_bps':-820.2,'profit_factor':0.0}})
     assert x['classification']=='ECONOMIC_EVIDENCE_NEGATIVE'
+    print('PASS_G5_PIPELINE_WATCHDOG_REGRESSION')
+
+
+if __name__ == '__main__':
+    run()
